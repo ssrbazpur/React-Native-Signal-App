dca7ebc006dbb84f4521a8fb573a4b7e05c2c7a3
ssrbazpur <ssrbazpur@gmail.com>
2021-02-16 21:35:32 -0600

    :tada: Add RegisterScreen Component

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCn2QHsYTZbhQfMsfhpH3x19SLbEdTmtIU",
+  authDomain: "awesomeproject-ecfd6.firebaseapp.com",
+  projectId: "awesomeproject-ecfd6",
+  storageBucket: "awesomeproject-ecfd6.appspot.com",
+  messagingSenderId: "1040574053588",
+  appId: "1:1040574053588:web:705853645e83c302054f41",
+};
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
 import { Button, Input, Text, Image } from "react-native-elements";
 
@@ -7,6 +7,13 @@ const RegisterScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: "Back to Login",
+    });
+  }, [navigation]);
+
   const register = () => {};
   return (
     <KeyboardAvoidingView behaviour="padding" style={styles.container}>
